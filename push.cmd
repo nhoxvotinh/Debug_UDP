1b3c78b56f2e5222bdcf5e9a7cdb52dbe49ca3e2
NhoxVoTinh <xuankhai82@gmail.com>
2022-09-27 11:48:57 +0700

    oke

--- push.cmd
+++ push.cmd
@@ -1,3 +1,10 @@
+@echo off
+set "par1=%~1"
+goto :par1Check
+:push
+set /p "par1=Enter Name Commit: "
+:par1Check
+if "%par1%"=="" goto :push
 git add .
-git commit -m "oke"
+git commit -m %par1%
 git push
